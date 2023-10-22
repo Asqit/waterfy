5f22f30e45f7ac9cd0b04ef0d81146dd3f67aced
Ondřej Tuček <ondrejtucek9@gmail.com>
2023-10-22 12:41:15 +0200

    refactoring

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # waterfy
 
-![sample_notification](notification.png)
+![sample_notification](./repo/notification.png)
 
 The sentence "I have a problem with drinking" might sound obvious, but actually I mean it the opposite way of yours. I don't feel the need to drink and to maintain my health I have created this stupidly simple script to remind me. 
 
 To run it, you must create a service that runs this script on startup of your computer. 
+
+
+## Dependecies
+
+1. Plyer - tool for accessing underlaying OS & HW
--- main.py
+++ main.py
@@ -1,73 +1,110 @@
-import sched
-import time
-import json
-import os
+from json import load, JSONDecodeError, dump
 from plyer import notification
+from platform import system
+from sched import scheduler
+from time import time, sleep
+import os
 
 
-def read_config(file_path: str) -> str:
-    """
-    A function that parses a JSON config file
+def get_config_path() -> str:
+    result: str = ""
 
-    Arguments:
-        file_path: a string containing a path of the config file
-    Returns:
-        the parsed configuration file or None
-    """
+    if system() == "Windows":
+        result = "C:\\ProgramData\\waterfy\\"
+    else:
+        result = str(os.getenv("HOME")) + "/.waterfy/"
 
-    assert type(file_path) == str, f"Invalid parameter type."
+    return result
+
+
+def get_corrected_minutes() -> int:
+    while True:
+        minutes = int(input("Enter amount of minutes between reminders: "))
+
+        if minutes <= 0:
+            print("Invalid value, please try again")
+            continue
+
+        return minutes
+
+
+def create_configuration() -> None:
+    minutes = get_corrected_minutes()
+    path = get_config_path()
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    data = {"frequency": minutes}
 
     try:
-        with open(file_path, "r") as file:
-            data = json.load(file)
-            return data
+        with open(file=path + "waterfy.json", mode="w") as cfg:
+            dump(data, cfg, ensure_ascii=False, indent=4)
     except FileNotFoundError:
-        print("Config file was not found")
-    except json.JSONDecodeError:
-        print("Failed to parse the JSON config")
-    return None
+        print("Failed to write to a file")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    else:
+        return
+
+
+def load_configuration(file_path: str) -> dict | None:
+    """
+    A function that parses a JSON configuration file
+    Arguments:
+        file_path {str}: a path of the config file
+    Returns:
+        parsed config or None
+    """
+
+    assert isinstance(file_path, str), "Invalid parameter type, expected a string"
+
+    if not os.path.exists(file_path) or not os.path.isfile(file_path):
+        print("No configuration file found, creating a new one")
+        create_configuration()
+        return
+
+    try:
+        with open(file=file_path, mode="r") as raw_file:
+            parsed_json = load(raw_file)
+    except FileNotFoundError:
+        print("No configuration file was found, creating a new one")
+        create_configuration()
+    except JSONDecodeError:
+        print("failed to parse the JSON config")
+    except Exception as e:
+        print(f"Unknown exception occurred: {e}")
+    else:
+        return parsed_json
 
 
 def get_notification_frequency() -> int:
-    """
-    A function that retrieves a frequency of each notification
-    Returns:
-        a frequency of each notification
-    """
-
-    APP_DATA_PATH = os.getenv("TEMP")
-    CONFIG_PATH = os.path.join(APP_DATA_PATH, "waterfy.json")
-
-    config = read_config(CONFIG_PATH)
+    config = load_configuration(get_config_path() + "waterfy.json")
 
     if config == None:
-        raise Exception("Failed to load config file")
-
-    return config["waterfy_frequency"]
-
-
-HOUR_IN_SECS = 3600
-WATERFY_DELAY = HOUR_IN_SECS * get_notification_frequency()
-EVENT_SCHEDULE = sched.scheduler(time.time, time.sleep)
+        raise Exception("Failed to load configuration")
+    else:
+        return config.get("frequency")
 
 
-def notify() -> None:
-    """A function that pushes notification"""
+EVENT_SCHEDULER = scheduler(time, sleep)
+NOTIFICATION_FREQUENCY = get_notification_frequency() * 60
 
+
+def push_notification() -> None:
     notification.notify(
         app_name="Waterfy",
-        app_icon=os.getcwd() + "\icon.ico",
+        app_icon=os.getcwd() + "\\assets\icon.ico",
         title="Waterfy",
         message="It is time to drink some water",
         timeout=2,
     )
-    EVENT_SCHEDULE.enter(WATERFY_DELAY, 1, notify)
+    EVENT_SCHEDULER.enter(NOTIFICATION_FREQUENCY, 1, push_notification)
 
 
 def init() -> None:
-    """A function that starts the program"""
-    EVENT_SCHEDULE.enter(1, 1, notify)
-    EVENT_SCHEDULE.run()
+    EVENT_SCHEDULER.enter(1, 1, push_notification)
+    EVENT_SCHEDULER.run()
 
 
 if __name__ == "__main__":
